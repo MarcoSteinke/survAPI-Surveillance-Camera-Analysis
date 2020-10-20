062dad5591f9b4cf0b11b503b639dfc8559a103b
maste150hhu <mastremolten2@gmail.com>
2020-10-20 22:34:35 +0200

    implement objectDetection

--- objectDetection.js
+++ objectDetection.js
@@ -1,7 +1,9 @@
-let img, detector;
+let img;
+let detector;
 
 function preload() {
     img = loadImage('https://images2.minutemediacdn.com/image/upload/c_crop,h_835,w_1254,x_1,y_0/v1554995050/shape/mentalfloss/516438-istock-637689912.jpg?itok=SkYIK_Ob');
+    detector = ml5.objectDetector('cocossd');
 }
 
 function setup() {
