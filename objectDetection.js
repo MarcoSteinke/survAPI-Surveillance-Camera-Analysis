90316b0dff057163c7e04324e8dda3e38bc9a11f
maste150hhu <mastremolten2@gmail.com>
2020-10-21 12:33:30 +0200

    update frontend by adding notification

--- objectDetection.js
+++ objectDetection.js
@@ -1,12 +1,19 @@
 let img;
 let detector;
+let objects = [];
 
 function preload() {
     img = loadImage('https://images2.minutemediacdn.com/image/upload/c_crop,h_835,w_1254,x_1,y_0/v1554995050/shape/mentalfloss/516438-istock-637689912.jpg?itok=SkYIK_Ob');
-    detector = ml5.objectDetector('cocossd');
+    detector = ml5.objectDetector('cocossd', modelLoaded);
+}
+
+function modelLoaded() {
+    document.querySelector("#state").className = "loaded";
+    document.querySelector("#state").innerHTML = "Model loaded.";
 }
 
 function setup() {
     createCanvas(1254, 835);
     image(img, 0, 0);
+
 }
