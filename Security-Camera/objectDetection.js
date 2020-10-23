617e6411a33ad79e5f2d02765e3bb47345b8cc57
maste150hhu <mastremolten2@gmail.com>
2020-10-23 11:25:34 +0200

    draw object frames

--- Security-Camera/objectDetection.js
+++ Security-Camera/objectDetection.js
@@ -4,6 +4,7 @@ let objects = [];
 
 function preload() {
     video = createCapture(VIDEO);
+    video.hide();
     detector = ml5.objectDetector('cocossd', modelLoaded);
 }
 
@@ -15,19 +16,35 @@ function modelLoaded() {
 function setup() {
     createCanvas(1254, 835);
     image(video, 0, 0);
+
+}
+
+function detect() {
+    detector.detect(video, (error, result) => {
+        objects = result;
+    });
 }
 
 function draw() {
 
-    objects.forEach( object => {
-        text(object.label, object.x, object.y - 10);
+    clear();
 
-        stroke(0, 255, 0);
-        line(object.x, object.y, object.x + object.width, object.y);
-        line(object.x, object.y, object.x, object.y + object.height);
-        line(object.x, object.y + object.height, object.x + object.width, object.y + object.height);
-        line(object.x + object.width, object.y, object.x + object.width, object.y + object.height);
+    detect();
 
-        stroke(0,0,0);
-    });
+    console.log(objects);
+
+    if(objects && objects.length > 0) {
+        objects.forEach( object => {
+            text(object.label, object.x, object.y - 10);
+    
+            stroke(0, 255, 0);
+            line(object.x, object.y, object.x + object.width, object.y);
+            line(object.x, object.y, object.x, object.y + object.height);
+            line(object.x, object.y + object.height, object.x + object.width, object.y + object.height);
+            line(object.x + object.width, object.y, object.x + object.width, object.y + object.height);
+    
+            stroke(0,0,0);
+        });
+    }
+    
 }
