031c7960ca55969ba7e0bf4e56a051a2dc8328a5
maste150hhu <mastremolten2@gmail.com>
2021-03-06 18:09:33 +0100

    add new classes

---
+++ Camera.js
@@ -0,0 +1,9 @@
+class Camera {
+
+    constructor(id, name, description, configuration) {
+        this.id = id;
+        this.name = name;
+        this.description = description;
+        this.configuration = configuration;
+    }
+}
---
+++ CameraConfiguration.js
@@ -0,0 +1,5 @@
+class CameraConfiguration {
+
+    //TODO: later
+    constructor()
+}
