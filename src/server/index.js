f5e26cb10ecf5a53b8008502ddd5253896501a4e
maste150hhu <mastremolten2@gmail.com>
2021-03-07 17:41:52 +0100

    add cors

--- src/server/index.js
+++ src/server/index.js
@@ -2,5 +2,9 @@ const express = require('express');
 const app = express();
 const port = 3000;
 
+const cors = require('cors');
+
+app.use(cors());
+
 app.get('/', (req, res) => res.json(1));
 app.listen(port, () => console.log(`Example app listening on port ` + port));
