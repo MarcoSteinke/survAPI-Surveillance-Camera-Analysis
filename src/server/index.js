c5e621f545ba0d015fbfe3e9e81f3078c22b4d7d
maste150hhu <mastremolten2@gmail.com>
2021-03-07 17:40:42 +0100

    make server restful

--- src/server/index.js
+++ src/server/index.js
@@ -2,5 +2,5 @@ const express = require('express');
 const app = express();
 const port = 3000;
 
-app.get('/', (req, res) => res.send('Surveillance application started'));
+app.get('/', (req, res) => res.json(1));
 app.listen(port, () => console.log(`Example app listening on port ` + port));
