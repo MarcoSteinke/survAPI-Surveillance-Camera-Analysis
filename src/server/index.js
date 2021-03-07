0bba68a9b08fe509c05d01ce5f4c0cda58948855
maste150hhu <mastremolten2@gmail.com>
2021-03-07 17:57:52 +0100

    add sequelize and mysql2

--- src/server/index.js
+++ src/server/index.js
@@ -6,5 +6,24 @@ const cors = require('cors');
 
 app.use(cors());
 
+const { Sequelize } = require('sequelize');
+
+// Option 2: Passing parameters separately (other dialects)
+const sequelize = new Sequelize('detections', 'root', 'example', {
+    host: 'localhost',
+    dialect: 'mysql'
+});
+
+
+
 app.get('/', (req, res) => res.json(1));
-app.listen(port, () => console.log(`Example app listening on port ` + port));
+app.listen(port, () => checkDatabaseConnection());
+
+async function checkDatabaseConnection() {
+    try {
+        await sequelize.authenticate();
+        console.log('Connection has been established successfully.');
+    } catch (error) {
+        console.error('Unable to connect to the database:', error);
+    }
+}
