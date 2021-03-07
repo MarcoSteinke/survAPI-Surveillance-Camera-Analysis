7b3bfd8b5d0c9ac46d58f06707fc9efe469108e1
maste150hhu <mastremolten2@gmail.com>
2021-03-07 14:24:44 +0100

    add nodejs server

---
+++ src/server/index.js
@@ -0,0 +1,6 @@
+const express = require('express')
+const app = express()
+const port = 3000
+
+app.get('/', (req, res) => res.send('Surveillance application started'))
+app.listen(port, () => console.log(`Example app listening on port ` + port))
