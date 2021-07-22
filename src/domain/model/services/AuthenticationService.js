f96fb410211fc66bc32a9dbe91aae81e5184d4ba
maste150hhu <mastremolten2@gmail.com>
2021-07-22 17:39:49 +0200

    add documentation to authenticationservice

--- src/domain/model/services/AuthenticationService.js
+++ src/domain/model/services/AuthenticationService.js
@@ -2,6 +2,13 @@
 const bcrypt = require('bcrypt');
 const saltRounds = 10;
 
+/**
+ * 
+ * @param {String} password entered password
+ * @param {String} userPassword persisted user password
+ * @param {Response} res Response in which the website will be rendered
+ * @param {Session} sessionTmp current user session
+ */
 exports.authenticate = async function(password, userPassword, res, sessionTmp) { 
     
     bcrypt.compare(password, userPassword, function(err, result) {
